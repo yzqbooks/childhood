89e3c170a1972fdc1bbbfcedd2e37808b7e7d772
yzqdev <yzqdev@outlook.com>
2022-04-11 18:19:48 +0800

    添加学前班的事情

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,13 @@
+import { defineNavbarConfig } from "vuepress-theme-hope";
+
+export default defineNavbarConfig([
+  // { text: "文章", icon: "home", link: "/article/" },
+
+  { text: "学前班", link: "/学前班/" },
+
+  { text: "小学", link: "/小学/" },
+  { text: "初中", link: "/初中/" },
+  { text: "高中", link: "/高中/" },
+  { text: "大学", link: "/大学/" },
+  { text: "工作后", link: "/工作后/" },
+]);
---
+++ docs/.vuepress/sidebar.ts
@@ -0,0 +1,11 @@
+import { defineSidebarConfig } from "vuepress-theme-hope";
+
+export default defineSidebarConfig({
+  "/学前班/": "structure",
+
+  "/小学/": "structure",
+  "/初中/": "structure",
+  "/高中/": "structure",
+  "/大学/": "structure",
+  "/工作后/": "structure",
+});
